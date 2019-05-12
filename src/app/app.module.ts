fb7bfeb46323d924ea2c52735e29c20ecad74c5e
Andersson <andersonj.urbinav@gmail.com>
2019-05-12 18:29:27 -0500

    Photos Gallery

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { BsDropdownModule, TabsModule } from 'ngx-bootstrap';
 import { RouterModule } from '@angular/router';
+import { JwtModule } from '@auth0/angular-jwt';
+import { NgxGalleryModule } from 'ngx-gallery';
 
 // Import Components owner
 import { AppComponent } from './app.component';
@@ -20,12 +22,25 @@ import { MessagesComponent } from './messages/messages.component';
 import { AuthService } from './services/auth.service';
 import { ErrorInterceptorProvide } from './services/error.interceptor';
 import { AlertifyService } from './services/alertify.service';
+import { UserService } from './services/user.service';
 
 // Import Routes owner
 import { appRoutes } from './routes';
 
 // Import Guards owner
 import { AuthGuard } from './guards/auth.guard';
+import { MemberCardComponent } from './member-card/member-card.component';
+import { MemberDetailComponent } from './member-detail/member-detail.component';
+
+// Import Resolvers owner
+import { MemberDetailResolver } from './resolvers/member-detail.resolver';
+import { MemberListResolver } from './resolvers/member-list.resolver';
+import { from } from 'rxjs';
+
+
+export function tokenGetter() {
+  return localStorage.getItem('token');
+}
 
 
 @NgModule({
@@ -36,7 +51,9 @@ import { AuthGuard } from './guards/auth.guard';
     RegisterComponent,
     MemberListComponent,
     ListsComponent,
-    MessagesComponent
+    MessagesComponent,
+    MemberCardComponent,
+    MemberDetailComponent
   ],
   imports: [
     BrowserModule,
@@ -44,13 +61,25 @@ import { AuthGuard } from './guards/auth.guard';
     HttpClientModule,
     FormsModule,
     BsDropdownModule.forRoot(),
-    RouterModule.forRoot(appRoutes)
+    TabsModule.forRoot(),
+    RouterModule.forRoot(appRoutes),
+    NgxGalleryModule,
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: tokenGetter,
+        whitelistedDomains: ['localhost:5000'],
+        blacklistedRoutes: ['localhost:5000/api/auth']
+      }
+    })
   ],
   providers: [
     AuthService,
     ErrorInterceptorProvide,
     AlertifyService,
-    AuthGuard
+    AuthGuard,
+    UserService,
+    MemberDetailResolver,
+    MemberListResolver
   ],
   bootstrap: [AppComponent]
 })
--- src/app/member-list/member-list.component.ts
+++ src/app/member-list/member-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../models/user';
+import { UserService } from '../services/user.service';
+import { AlertifyService } from '../services/alertify.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-member-list',
@@ -7,9 +11,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MemberListComponent implements OnInit {
 
-  constructor() { }
+  users: User[];
+
+  constructor( private userService: UserService, private alertify: AlertifyService, private route: ActivatedRoute ) { }
 
   ngOnInit() {
+    this.route.data.subscribe( data => {
+      this.users = data['users'];
+    });
   }
+
+  /*loadUsers() {
+    this.userService.getUsers().subscribe( ( users: User[] ) => {
+      this.users = users;
+    }, error => {
+      this.alertify.error( error );
+    });
+  }*/
 
 }
